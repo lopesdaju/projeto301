7f9c1d1ba281f6dd73e665de0af4297b295d2c87
unknown <lopesgl94311@gmail.com>
2025-10-23 13:12:07 -0300

    criação da pagina inicial

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>to_do_list</title>
+    <link rel = "stylesheet" href = "style.css">
+</head>
+<body>
+    <h1> Minha to_do_list </h1>
+    <form action = "add_task.php" method = "POST" >
+    <input type = "text" name = "title" placeholder = "Nova tarefa" required>
+    <button type= "submit"> Adicionar </button>
+</form>
+    
+</body>
+</html>
